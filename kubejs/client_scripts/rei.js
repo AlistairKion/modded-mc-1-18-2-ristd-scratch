b8af2ee8170fbcf85323a67a4dc844292b83b7ea
AJ Snarr <ajsnarr98@gmail.com>
2023-06-26 00:52:10 -0400

    balance some things and remove apotheosis sound

--- kubejs/client_scripts/rei.js
+++ kubejs/client_scripts/rei.js
@@ -1,7 +1,8 @@
 // priority: 0
 console.info('AQM3 -- Removing Items.... v1.0');
 const removeJei = [
-  "functionalstorage:ender_drawer"
+  "functionalstorage:ender_drawer",
+  "projecte:rm_katar"
 ];
 
 onEvent('rei.hide.items', event => {
--- kubejs/server_scripts/balance.js
+++ kubejs/server_scripts/balance.js
@@ -3,3 +3,10 @@
 onEvent('recipes', event => {
 	event.remove({ output: "functionalstorage:ender_drawer" })
 })
+
+onEvent('recipes', event => {
+	event.remove({ output: "projecte:rm_katar" })
+})
+onEvent('recipes', event => {
+	event.remove({ output: "projecte::rm_sword" })
+})
